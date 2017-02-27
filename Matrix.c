1a7f745526dfe92c7b186022c992c8ffdbf9f457
chongc <Chongc@student.wpunj.edu>
2017-02-27 16:23:09 -0500

    Set up sode to solve TDSE

--- Matrix.c
+++ Matrix.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void Commutator(int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]);
+void Commutator (int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]);
 int main() {
 
 int dim = 5;
@@ -10,7 +10,6 @@ double H[dim][dim], D[dim][dim], Pa[dim][dim], Pb[dim][dim];
 
 D[1][1] = 3;
 D[0][1] = 1;
-
 H[1][1] = 1.0;
 H[0][0] = 1.5;
 
@@ -20,7 +19,7 @@ for (int i=0; i<dim; i++) {
 
     double sum2 = 0;
     double sum1 = 0; 
-    for (int k=0; k<2; k++) {
+    for (int k=0; k<dim; k++) {
    
       sum2 += H[i][k]*D[k][j];
       sum1 += D[i][k]*H[k][j];
@@ -36,8 +35,27 @@ for (int i=0; i<dim; i++) {
 
 }
 
-void Commutator(int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]) {
+void Commutator (int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]) {
 
 // write code here!
+ for (int i=0; i<dim; i++) {
+
+   for (int j=0; j<dim; j++) {
+
+   for(int k=0; k<dim; ++k)
+			{
+      sum2 += H[i][k]*D[k][j];
+      sum1 += D[i][k]*H[k][j];
+    }
+    Pa[i][j] = sum1;
+      printf("  Pa[%i][%i] is %f\n",i,j,sum1);
+
+     Pb[i][j] = sum2;
+      printf(" Pb[%i][%i] is %f\n",i,j,sum2);
+      
+
 
 }
+}
+}
+
