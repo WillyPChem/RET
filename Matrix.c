32aa502407487f18968f73b1ce4c8af52c136d8c
chongc <Chongc@student.wpunj.edu>
2017-03-03 15:03:42 -0500

    Adjusted code for denisty matrix

--- Matrix.c
+++ Matrix.c
@@ -2,20 +2,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
+#include<complex.h>
 
-<<<<<<< HEAD
-void Commutator (int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]);
-=======
-void Commutator(int dim, double *H, double *D, double *P);
->>>>>>> 6429e597c5a2793ad4511947767feccf9a440da7
+//void Commutator (int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]);
+
+void Commutator(int dim, double *H, double complex *D, double complex *P);
+double pi = 3.14159625;
 int main() {
 
-int dim = 2;
-double *H, *D, *P;
+int dim = 2.;
+double *H;
+double complex *D, *P;
 
 H = (double *)malloc(dim*dim*sizeof(double));
-D = (double *)malloc(dim*dim*sizeof(double));
-P = (double *)malloc(dim*dim*sizeof(double));
+D = (double complex *)malloc(dim*dim*sizeof(double complex));
+P = (double complex *)malloc(dim*dim*sizeof(double complex));
 
 // Element DM(0,0) = D(0*dim+0)
 // Element DM(1,0) = D(1*dim+0)
@@ -30,11 +31,11 @@ H[1][1] = 1.0;
 H[0][0] = 1.5;
 */
 
-D[1*dim + 1] = 3;
-D[0*dim + 1] = 1;
+D[0*dim + 0] = 1/2.;
+D[1*dim + 1] = 1/2.;
 
-H[1*dim + 1] = 1;
-H[0*dim + 0] = 1.5;
+H[0*dim + 0] = (pi*pi/2);
+H[1*dim + 1] = 2*pi*pi;
 
 
 Commutator( dim, H, D,  P);
@@ -73,8 +74,8 @@ for (int i=0; i<dim; i++) {
 
 }
 
-<<<<<<< HEAD
-void Commutator (int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]) {
+
+/*void Commutator (int dim, double H[dim][dim], double D[dim][dim], double P[dim][dim]) {
 
 // write code here!
  for (int i=0; i<dim; i++) {
@@ -91,29 +92,29 @@ void Commutator (int dim, double H[dim][dim], double D[dim][dim], double P[dim][
 
      Pb[i][j] = sum2;
       printf(" Pb[%i][%i] is %f\n",i,j,sum2);
-      
+*/      
 
-=======
-void Commutator(int dim, double *H, double *D, double *P) {
+
+void Commutator(int dim, double *H, double complex *D, double complex *P) {
 
 // write code here!
 for (int i=0; i<dim; i++) {
 
   for (int j=0; j<dim; j++) {
->>>>>>> 6429e597c5a2793ad4511947767feccf9a440da7
 
-    double sum2 = 0;
-    double sum1 = 0;
+
+    double complex sum2 = 0+0*I;
+    double complex sum1 = 0+0*I;
     for (int k=0; k<dim; k++) {
 
-      sum1 += H[i*dim+k]*D[k*dim+j];
-      sum2 += D[i*dim+k]*H[k*dim+j];
+      sum1 += H[i*dim+k]*D[k*dim+j]*I;
+      sum2 -= D[i*dim+k]*H[k*dim+j]*I;
     }
     P[i*dim+j] = sum1 - sum2;
-    printf(" Pb[%i][%i] is %f\n",i,j,sum1-sum2);
+    printf(" Pb[%i][%i] is %f %f\n",i,j,creal(sum1-sum2),cimag(sum1-sum2));
 }
 }
 }
-}
-}
+
+
 
