7d9f73c6fad858587b5c8058fc67e3539a63e6db
chongc <Chongc@student.wpunj.edu>
2017-04-10 16:12:34 -0400

    Second Updated Test.eps

--- Matrix.c
+++ Matrix.c
@@ -159,7 +159,7 @@ for (int i=0; i<dim; i++) {
 //void RK3(int dim, double *xvec, double complex *wfn, double dx, double dt)
 
 
-for (int j=0; j<50000; j++) {
+for (int j=0; j<500000; j++) {
 
   
 
