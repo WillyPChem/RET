801c4f3631c889c3e5d5177572f78187ba8413b7
WillyPChem <willypchem@gmail.com>
2017-08-10 10:46:58 -0400

    Update CoupledDensityMatrix.c

--- CoupledDensityMatrix.c
+++ CoupledDensityMatrix.c
@@ -336,7 +336,8 @@ int main() {
   
   
   printf("  Max(||D(0)-D(t)||) is %12.10e at t=%12.10e\n",max_MG_Error,TransferTime);
-  printf("  r_x      r_y     r_z      Energy Transferred (eV) \n");
+
+  printf("  r_x          r_y         r_z          Energy Transferred (eV) \n");
   printf("  %6.3e    %6.3e   %6.3e    %12.10e eV\n",r[0],r[1],r[2],E_Transfer*27.211);
 
   for (int i=numTime; i<zeropad; i++) {
